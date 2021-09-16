255369c3c152fc76414dce68e3cbcdee2715a124
Ilan Herbach <ilan.herbach@gmail.com>
2021-09-15 21:34:34 -0300

    With dummy DB, user registration is validated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 
-app.listen(3333);
+const customers = [];
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerExists = customers.some((costumer) => costumer.cpf === cpf);
+
+  if (customerExists) {
+    return response.status(400).json({
+      error: 'Customer already exists.',
+    });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => console.log(`Started at port ${PORT}`));
